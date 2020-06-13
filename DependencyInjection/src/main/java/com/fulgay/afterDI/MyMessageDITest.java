828065cd6d60cd7e952215e672d71d71bd3c46da
Fırat Ülgay <56871921+firatulgay@users.noreply.github.com>
2020-06-13 17:43:14 +0300

    Update MyMessageDITest.java

--- DependencyInjection/src/main/java/com/fulgay/afterDI/MyMessageDITest.java
+++ DependencyInjection/src/main/java/com/fulgay/afterDI/MyMessageDITest.java
@@ -14,7 +14,7 @@ import com.fulgay.afterDI.InjectorsClasses.SmsServiceInjector;
 
 /**
  * Burada görüldüğü üzere şu andan itibaren application class'larımız sadece serviceleri kullanmaktan sorumlu.
- * Diğer yandan ileride uygulamamız örneğin facebook mesajı da göndersin istersek tek yapılması gereken Service ce Injector classlarının yazılmasıdır.
+ * Diğer yandan ileride uygulamamız örneğin facebook mesajı da göndersin istersek tek yapılması gereken Service ve Injector classlarının yazılmasıdır.
  * Böylelikle Dependecy Injection Design Pattern ile bağımlılıkları azaltıp aynı zamanda genişletilebilir de bir yapı kurmuş olduk.
  *
  */
